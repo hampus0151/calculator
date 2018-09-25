dd8a031c3ef1a8a1579f55276a05e8d8a3c0f684
Hampus Nasvall <hampus.nasvall@gmail.com>
2018-09-25 19:44:17 +0200

    else delete

--- calc.java
+++ calc.java
@@ -40,7 +40,7 @@ public class calc
             System.out.println("your answer is " + (num1 + num2));
         }
 
-        else if (Objects.equals(operation,"-"))
+        if (Objects.equals(operation,"-"))
         {
             System.out.println("your answer is " + (num1 - num2));
         }
